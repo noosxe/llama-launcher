912cfd6a21472b22135ed6f3ce27f70a0fa7763a
Levon Kirakosyan <noosx.e@gmail.com>
2026-04-21 14:04:01 +0400

    feat: introduce --version global flag (#5)

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -8,11 +8,13 @@ import (
 )
 
 var cfgFile string
+var Version = "dev"
 
 var rootCmd = &cobra.Command{
-	Use:   "llama-launcher",
-	Short: "Launch llama.cpp containers with preconfigured models",
-	Long:  `A CLI tool to manage and launch llama.cpp Docker containers with predefined model configurations.`,
+	Use:     "llama-launcher",
+	Short:   "Launch llama.cpp containers with preconfigured models",
+	Long:    `A CLI tool to manage and launch llama.cpp Docker containers with predefined model configurations.`,
+	Version: Version,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("llama-launcher: Use --help for usage information")
 	},
@@ -27,6 +29,8 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+
+	rootCmd.SetVersionTemplate("{{.Name}} version {{.Version}}\n")
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $XDG_CONFIG_HOME/llama-launcher/config.toml or ./config.toml)")
 }
